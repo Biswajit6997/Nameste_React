2c10bc63d113959abb6c3a48b4e8dce52a5be6b3
Biswajit6997 <biswajitde1996@gmail.com>
2024-03-22 19:20:30 +0530

    add file

---
+++ 03_Laying the foundation/App.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+//React Element
+const heading=React.createElement("h1",{id:"head"},"Nameste React🚀🚀")
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
